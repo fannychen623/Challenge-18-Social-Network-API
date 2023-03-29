2cdea21eae3a9398d31d640f0a20d150066bd247
fycn <fannychen623@gmail.com>
2023-03-28 20:52:45 -0400

    added update thought request

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -32,6 +32,22 @@ module.exports = {
       ).exec();
     });   
   },
+
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this id!' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  }, 
+
   // Delete a thought and remove them from the user
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
